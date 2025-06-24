c7375420e6fb34b5e0fdb6d3c1f537877c891f1f
stephenhallett <stephenhallett@kpmg.co.nz>
2025-06-24 15:06:04 +1200

    feat: Add models dockerfile/compose

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 import litserve as ls
 
 from .NearestNeighbours.VotingNN import VotingNN
@@ -5,4 +7,4 @@ from .NearestNeighbours.VotingNN import VotingNN
 if __name__ == "__main__":
     nn1_model = VotingNN(api_path="/1nn")
     server = ls.LitServer(lit_api=[nn1_model], accelerator="auto")
-    server.run(port=8000)
+    server.run(port=os.environ["MODELS_PORT"])
