fb6ed5f8d5a5ad716ede42a836d714a110d32dcd
Stephen-Hallett <stevohallett@gmail.com>
2025-08-02 17:27:27 +1200

    feat: Start building out summary endpoint models

---
+++ database/schemas/summary.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Transaction(BaseModel):
+    id: str
+    account: str
+    user_id: str
+    segment_id: str
+    hash: str
+    date: datetime
+    type: str
+    amount: float
+    description: str
+    category: str | None
+    group_name: str | None
+    merchant: str | None
+    colour: str
+    confirmed: bool
+
+
+class Metrics(BaseModel):
+    spent: float
+    income: float
+    percentage: float
